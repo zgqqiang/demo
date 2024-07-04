a413e5ce006eea872d6bc4cd5690d84312830b01
guoqiang.zuo@gientech.com <zgq@995851>
2024-07-04 16:30:32 +0800

    demo k8s-service

--- go-start.sh
+++ go-start.sh
@@ -4,7 +4,7 @@
 wget https://golang.google.cn/dl/go1.21.4.linux-amd64.tar.gz
 
 # 解压安装包到/usr/local
-tar -C /usr/local -zxvf go1.21.4.linux-amd64.tar.gz
+tar -C /usr/local -zxf go1.21.4.linux-amd64.tar.gz
 
 # 在/etc/profile文件末尾添加环境变量配置
 echo 'export GOROOT=/usr/local/go' >> /etc/profile
@@ -18,3 +18,14 @@ go env -w GOPROXY=https://goproxy.cn,direct
 go env -w GO111MODULE=on
 
 echo "Go安装和配置完成!"
+
+# 下载Helm版本，以3.5.3为例
+wget https://get.helm.sh/helm-v3.5.3-linux-amd64.tar.gz
+ 
+# 解压缩
+tar -zxf helm-v3.5.3-linux-amd64.tar.gz
+ 
+# 移动并给予执行权限
+sudo mv linux-amd64/helm /usr/local/bin/helm
+sudo chmod +x /usr/local/bin/helm
+echo "helm安装和配置完成!"
---
+++ k8s-service/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	_ "github.com/apache/skywalking-go"
+	"github.com/gin-gonic/gin"
+)
+
+type sliceValue []string
+
+func (s *sliceValue) Set(val string) error {
+	values := strings.Split(val, ",")
+	*s = append(*s, values...)
+	return nil
+}
+
+func (s *sliceValue) String() string {
+	return strings.Join(*s, ",")
+}
+
+var (
+	serviceList      sliceValue
+	service          string
+	code             int
+	clientTimeout    int
+	serverHandleTime int
+)
+
+func init() {
+	flag.StringVar(&service, "service", "servicea", "service name")
+	flag.Var(&serviceList, "serviceList", "servicea,serviceb,servicec")
+	flag.IntVar(&code, "code", 200, "response return code")
+	flag.IntVar(&clientTimeout, "clientTimeout", 10, "client time out")
+	flag.IntVar(&serverHandleTime, "serverHandleTime", 0, "simulate server processing time")
+	flag.Parse()
+}
+
+func main() {
+	serviceIndex := 0
+	for index := range serviceList {
+		if serviceList[index] == service {
+			serviceIndex = index
+			break
+		}
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		time.Sleep(time.Duration(serverHandleTime) * time.Second)
+
+		if len(serviceList) == serviceIndex+1 {
+			c.String(code, os.Getenv("POD_NAME"))
+			return
+		}
+		nextServiceName := serviceList[serviceIndex+1]
+
+		req, _ := http.NewRequest("GET", fmt.Sprintf("http://%s:8080/", nextServiceName), nil)
+		resp, err := (&http.Client{
+			Timeout: time.Duration(clientTimeout) * time.Second,
+		}).Do(req)
+		if err != nil {
+			fmt.Println("do request failed", err)
+			c.String(code, os.Getenv("POD_NAME"))
+			return
+		}
+
+		body, _ := ioutil.ReadAll(resp.Body)
+		c.String(code, os.Getenv("POD_NAME")+"-->"+string(body))
+	})
+
+	r.POST("/post", func(c *gin.Context) {
+		c.String(http.StatusOK, "Received a POST request")
+	})
+
+	r.Run(":8080")
+}
